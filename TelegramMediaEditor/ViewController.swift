0c56d771008505733e64e2076084c4b8808a9e4f
Vadzim Karonchyk <42875321+vadzimkaronchyk@users.noreply.github.com>
2022-10-09 20:33:59 +0300

    Create project

---
+++ TelegramMediaEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelegramMediaEditor
+//
+//  Created by Vadzim Karonchyk on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
